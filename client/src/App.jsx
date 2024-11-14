663503d44c964c80833a00e01dabdaebdb0c8fac
Manifest-Son <lennoxgithinji37@gmail.com>
2024-11-15 01:56:33 +0300

    This sets up our working environment.

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+// import DashboardLayout from './components/DashboardLayout'
+
+function App() {
+
+
+  return (
+    <>
+  
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/DashboardLayout.jsx
@@ -0,0 +1,40 @@
+// components/DashboardLayout.jsx
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import PropTypes from 'prop-types';
+import './DashboardLayout.css';
+
+const DashboardLayout = ({ menuItems, children, userRole }) => {
+  return (
+    <div className="dashboard-container">
+      <div className="sidebar">
+        <div className="user-info">
+          <h2>{userRole}</h2>
+        </div>
+        <nav className="menu">
+          {menuItems.map((item, index) => (
+            <div key={index} className="menu-item">
+              <i className={item.icon}></i>
+              <span>{item.label}</span>
+            </div>
+          ))}
+        </nav>
+      </div>
+      <main className="main-content">
+        {children}
+      </main>
+    </div>
+  );
+};
+DashboardLayout.propTypes = {
+  menuItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      icon: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  children: PropTypes.node.isRequired,
+  userRole: PropTypes.string.isRequired,
+};
+
+export default DashboardLayout;
